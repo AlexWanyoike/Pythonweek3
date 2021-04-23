1ad70a6b7677398ccb5de39d1de73fd8cfd40e16
AlexWanyoike <alex.wanyoike12@gmail.com>
2021-04-23 21:34:39 +0300

    BootStrap Created

--- app/main/views.py
+++ app/main/views.py
@@ -1,13 +1,29 @@
-from flask import render_template
+from flask import  Flask , render_template
+app = Flask(__name__)
 from app import app
+
+
+
+
 
 # Views
 @app.route('/')
+@app.route('/home')
 def index():
 
     '''
     View root page function that returns the index page and its data
     '''
 
-    message = 'Hello World'
-    return render_template('index.html',message = message)
+    message = 'Watu Wangu'
+    return render_template('base.html',message = message)
+
+@app.route('/about')
+def about():
+
+    '''
+    View root page function that returns the index page and its data
+    '''
+
+    about_page= 'Into the'
+    return render_template('about.html',about_page = about_page)
