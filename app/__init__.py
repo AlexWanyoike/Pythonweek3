0f876d91a5fdcff90a9cd8d4e7f5e0ded20c4e3a
AlexWanyoike <alex.wanyoike12@gmail.com>
2021-04-27 14:48:37 +0300

    Email and Models Created

--- app/__init__.py
+++ app/__init__.py
@@ -1,43 +1,40 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from flask_bootstrap import Bootstrap
 from config import config_options
-from flask import Blueprint
-#from .auth import auth as auth_blueprint
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_uploads import UploadSet,configure_uploads,IMAGES
+from flask_mail import Mail
 
-#app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
-
+bootstrap = Bootstrap()
+db = SQLAlchemy()
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
+photos = UploadSet('photos',IMAGES)
+mail = Mail()
 
 def create_app(config_name):
-    app = Flask(__name__, instance_relative_config= True)
-    
-    #bootstrap.init_app(app)
 
-    
-    
-    return app
+    app = Flask(__name__)
 
-#app.config['SECRET_KEY'] = 'd7b1a25b72b817d78afffe763942756b42b01564'
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-#db.init_app(app)
+    app.config.from_object(config_options[config_name])
+    config_options[config_name].init_app(app)
 
-auth = Blueprint('auth',__name__)
+    # Initializing flask extensions
+    bootstrap.init_app(app)
+    db.init_app(app)
+    login_manager.init_app(app)
+    mail.init_app(app)
 
+    # configure UploadSet
+    configure_uploads(app,photos)
 
+    # Registering the blueprints
+    from .main import main as main_blueprint
+    from .auth import auth as auth_blueprint
 
-#
-#from config import Config
-#from flask_migrate import Migrate
+    app.register_blueprint(main_blueprint)
+    app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
 
-
-#app = Flask(__name__)
-#db.init_app(app)
-#migrate = Migrate(app, db)
-
-#from app.models import User
-
-#app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-
-# Initializing application
-
-#from app import views
+    return 
--- app/models.py
+++ app/models.py
@@ -1,30 +1,63 @@
-from datetime import datetime
-from app import db
+from . import db
+from . import login_manager
+from flask_login import UserMixin,current_user
 from werkzeug.security import generate_password_hash,check_password_hash
-#No DB Import Created
 
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
-class User(db.Model):
+class User(UserMixin,db.Model):
     __tablename__ = 'users'
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    #image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
-    password = db.Column(db.String(60), nullable=False)
-    posts = db.relationship('Post', backref='author', lazy=True)
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(255))
+    email = db.Column(db.String(255),unique = True,index = True)
+    pass_secure = db.Column(db.String(255))
+    pitch = db.relationship('Pitch', backref='user', lazy='dynamic')
+    comment = db.relationship('Comment', backref = 'user', lazy = 'dynamic')
+
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
+
+    @password.setter
+    def password(self, password):
+        self.pass_secure = generate_password_hash(password)
+
+
+    def verify_password(self,password):
+        return check_password_hash(self.pass_secure,password)
+    
+    def __repr__(self):
+        return f'User {self.username}'
+
+class Pitch(db.Model):
+    __tablename__ = 'pitches'
+
+    id = db.Column(db.Integer, primary_key = True)
+    owner_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable = False)
+    description = db.Column(db.String(), index = True)
+    title = db.Column(db.String())
+    category = db.Column(db.String(255), nullable=False)
+    comments = db.relationship('Comment',backref='pitch',lazy='dynamic')
+    
+    @classmethod
+    def get_pitches(cls, id):
+        pitches = Pitch.query.order_by(pitch_id=id).desc().all()
+        return pitches
 
     def __repr__(self):
-        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+        return f'Pitch {self.description}'
 
 
-class Post(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100), nullable=False)
-    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-    content = db.Column(db.Text, nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+class Comment(db.Model):
+    __tablename__='comments'
+    
+    id = db.Column(db.Integer,primary_key=True)
+    pitch_id = db.Column(db.Integer, db.ForeignKey('pitches.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable= False)
+    description = db.Column(db.Text)
 
+    
     def __repr__(self):
-        return f"Post('{self.title}', '{self.date_posted}')"
-
-#db.create_all()
+        return f"Comment : id: {self.id} comment: {self.description}"
