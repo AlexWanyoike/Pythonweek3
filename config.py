745d32f0e779a9a7c6e46ed6741087d513d5b609
AlexWanyoike <alex.wanyoike12@gmail.com>
2021-04-27 15:59:35 +0300

    Manage.py and Config.py added

--- config.py
+++ config.py
@@ -1,37 +1,31 @@
+
+import os
+
 class Config:
-    '''
-    General configuration parent class
+    SECRET_KEY='d7b1a25b72b817d78afffe763942756b42b01564'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://ermoh:12345@localhost/pitch'
+    UPLOADED_PHOTOS_DEST='app/static/photos'
+    #  email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
 
-
-    '''
-    SECRET_KEY = 'd7b1a25b72b817d78afffe763942756b42b01564'
-
-    pass
-
+    @staticmethod
+    def init_app(app):
+        pass
 
 
 class ProdConfig(Config):
-    '''
-    Production  configuration child class
-
-    Args:
-        Config: The parent configuration class with General configuration settings
-    '''
     pass
 
 
 class DevConfig(Config):
-    '''
-    Development  configuration child class
-
-    Args:
-        Config: The parent configuration class with General configuration settings
-    '''
-
     DEBUG = True
 
-
 config_options = {
-'development': DevConfig,
-'production': ProdConfig
+'development':DevConfig,
+'production':ProdConfig
+
 }
