52f1d68dc71781d3a00c5bcd9bcd0df22d9b77ee
AlexWanyoike <alex.wanyoike12@gmail.com>
2021-04-29 12:25:55 +0300

    Git ignore

--- run.py
+++
@@ -1,9 +0,0 @@
-import os
-from app import create_app
-from flask_script import Manager, Shell , Server
-
-config_name = os.getenv('APP_SETTINGS')
-
-app = create_app(config_name)
-if __name__ == '__main__':
-    app.run(debug = True)
